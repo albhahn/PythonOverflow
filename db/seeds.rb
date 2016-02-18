f85dcaf1c18871945e5b3103b4a01cf35068e1dd
roche <email": null>
2016-02-18 14:42:08 -0800

    Add new seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,19 @@ Answer.delete_all
 Comment.delete_all
 
 #create 10 dummy users
-users = 10.times.map do
-  User.create!( :user_name => Faker::Internet.user_name,
+users = []
+10.times.map do
+  user = User.create!( :user_name => Faker::Internet.user_name,
                 :email      => Faker::Internet.email,
                 :password   => 'password' )
+  users << user
 end
 
 
 questions_seed = ["Who goes there?", "Where'd you get the coconuts?", "In order to maintain air-speed velocity, a swallow needs to beat its wings forty-three times every second, right?", "Well, how'd you become king, then?", "How do you know she is a witch?", "A newt?", "What... is your name?", "What... is your quest?", "What... is your favourite colour?", "What... is the air-speed velocity of an unladen swallow?"]
+
 questions = []
+
 questions_seed.each do |question|
   description = Faker::Hipster.paragraphs(2)
   this_question = Question.new(  title: question,
@@ -24,41 +28,91 @@ questions_seed.each do |question|
   questions << this_question
 end
 
-answers = 10.times.map do
-  text = Faker::Hipster.sentences(1)
+answers = []
 
+50.times.map do
+  question = questions[rand(0..9)]
+  user = users[rand(0..9)]
+
+  text = Faker::Hipster.sentences(1)
   answer = Answer.new( text: text)
-  answer.question = questions[rand(0..9)]
-  answer.user = users[rand(0..9)]
+
+  answer.user = user
+  question.answers << answer
+
+  question.save
+  user.save
+  answer.save
+
+  answers << answer
 end
 
-comments1 = 10.times.map do
+comments = []
+
+25.times do
+  question = questions[rand(0..9)]
+  user = users[rand(0..9)]
+
   text = Faker::Hipster.sentences(1)
   comment = Comment.new(text: text)
-  comment.user = users[rand(0..9)]
-  comment.commentable = questions[rand(0..9)]
+
+  comment.user = user
+  question.comments << comment
+
+  comment.save
+  user.save
+  question.save
+
+  comments << comment
 end
 
-comments2 = 10.times.map do
+25.times do
+  answer = answers[rand(0..9)]
+  user = users[rand(0..9)]
+
   text = Faker::Hipster.sentences(1)
   comment = Comment.new(text: text)
-  comment.user = users[rand(0..9)]
-  comment.commentable = answers[rand(0..9)]
+  comment.user = user
+  answer.comments << comment
+
+  answer.save
+  comment.save
+  user.save
+
+  comments << comment
 end
 
-votes1 = 100.times.map do
+votes = []
+
+100.times do
+  answer = answers[rand(0..9)]
+  user = users[rand(0..9)]
+
   vote = Vote.new()
-  vote.user = users[rand(0..9)]
-  vote.voteable = answers[rand(0..9)]
+  vote.user = user
+  answer.votes << vote
+
+  answer.save
+  vote.save
+  user.save
+
+  votes << vote
 end
 
-votes2 = 100.times.map do
+100.times do
+  question = questions[rand(0..9)]
+  user = users[rand(0..9)]
+
   vote = Vote.new()
-  vote.user = users[rand(0..9)]
-  vote.voteable = answers[rand(0..9)]
+  vote.user = user
+  question.votes << vote
+
+  question.save
+  vote.save
+  user.save
+
+  votes << vote
 end
-
-
 
 
 
