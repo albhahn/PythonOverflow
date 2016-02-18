e0364ddf5609d015eb0fb94adb9c7f1d4151105c
roche <email": null>
2016-02-18 15:45:41 -0800

    Add new question form to top of main page

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
   $('#ask-question').on("click", function(e){
     e.preventDefault();
-    console.log("click");
+    $(this).fadeOut(700, function(){
+    })
+    setTimeout(function(){
+      $('#new-question-form').fadeIn(700, function(){
+      })
+    }, 700)
+  })
+  $('#main-top').on("click", "#SPAM", function(e){
+    e.preventDefault();
+    var title = $('input[name="title"').val();
+    var description = $('input[name="description"').val();
+    var uri = $('form').attr("action");
+    $.ajax({url: uri, type: "POST", data: {title, description}}).done(function(response){
+      console.log(response);
+    })
   })
 
 });
